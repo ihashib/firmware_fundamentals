1ef0d9d58d5e257992c4c6f2f0296dceb7ee8364
Hashib Islam <ihashib2@gmail.com>
2021-09-05 13:18:19 +0600

    added arduio time counter with interrupt

---
+++ timer_with_interrupt/timer_with_interrupt.ino
@@ -0,0 +1,80 @@
+//Tutorial link: https://www.instructables.com/Arduino-Timer-Interrupts/
+
+int sec = 0, minute = 0;
+
+void setup(){
+  Serial.begin(9600);
+  //set pins as outputs
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
+
+cli();//stop interrupts
+
+
+//set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+
+//extra...............................................................
+//set timer0 interrupt at 2kHz
+  TCCR0A = 0;// set entire TCCR2A register to 0
+  TCCR0B = 0;// same for TCCR2B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 2khz increments
+  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS01 and CS00 bits for 64 prescaler
+  TCCR0B |= (1 << CS01) | (1 << CS00);   
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+
+//set timer2 interrupt at 8kHz
+  TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
+  TCNT2  = 0;//initialize counter value to 0
+  // set compare match register for 8khz increments
+  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS21 bit for 8 prescaler
+  TCCR2B |= (1 << CS21);   
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
+
+//....................................................................  
+
+
+
+sei();//allow interrupts
+
+}
+
+
+ISR(TIMER1_COMPA_vect){
+  sec++;
+  if( sec == 60 ){
+    minute++;
+    sec = 0;
+  }
+  Serial.print(minute); Serial.print(":"); Serial.println(sec);
+}
+  
+
+
+
+void loop(){
+  //do other things here
+  
+}
